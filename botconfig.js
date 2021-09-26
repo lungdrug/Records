b3bae3b6837f6a0cad0ee1fe453a6ebaff141ea4
Endifinity <maisonjroberts@gmail.com>
2021-09-26 13:24:09 +0100

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -3,7 +3,7 @@ module.exports = {
   ExpressServer: true,//If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "", //Support Server Link
+  SupportServer: "https://www.youtube.com/watch?v=dQw4w9WgXcQ", //Support Server Link
   Token: process.env.Token || "ODIxMDk4MDAyNTgzMzg4MjAw.YE-xGw.QlLA_rAu06mo-QVrOVgUgibsNjM", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "821098002583388200", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "SZ_xXkPKZljfWWEngW4iNH_xQjCZCOGm", //Discord Client Secret
