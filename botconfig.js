eddbf7c56ffc9ac8ee26eb9996bbd31aedeb94cf
TheH2006 <38357367+TheH2006@users.noreply.github.com>
2021-10-03 19:05:05 +0100

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -10,7 +10,7 @@ module.exports = {
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "recordsisnotbutitactuallyisverycool", //A Secret like a password
+  CookieSecret: "Pikachu is cute", //A Secret like a password
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
