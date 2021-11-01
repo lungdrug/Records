d4a6bef69c780094ff057ca669c2b0d463695298
Endifinity <maisonjroberts@gmail.com>
2021-11-01 10:50:47 +0000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = async (client) => {
   client.Ready = true, 
   client.user.setPresence({
-    status: "online",  // You can show online, idle, and dnd
+    status: "dnd",  // You can show online, idle, and dnd
     activity: {
         name: "a couple crackheads",  // The message shown
         type: "streaming", // PLAYING, WATCHING, LISTENING, STREAMING,
