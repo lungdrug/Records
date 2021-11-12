a1f180d6b19916b4b3fe96d75dbb83993910a2fd
Endifinity <maisonjroberts@gmail.com>
2021-11-12 18:31:08 +0000

    Revert "Update ready.js"
    This reverts commit 71bd63061e9f79b5634f39da52322d27694faa4c.

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ module.exports = async (client) => {
     status: "idle",  // You can show online, idle, and dnd
     activity: {
         name: "a couple crackheads",  // The message shown
-        type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
+        type: "streaming", // PLAYING, WATCHING, LISTENING, STREAMING,
     }
 });
     client.Manager.init(client.user.id);
